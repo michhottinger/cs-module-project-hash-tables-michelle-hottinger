8043c155f36f17dbde9e84a1cbbdd4e5b2056daa
michhottinger <michhottinger@users.noreply.github.com>
2020-07-09 16:21:02 -0700

    arkov was completed with help from online look up

--- applications/markov/markov.py
+++ applications/markov/markov.py
@@ -1,13 +1,48 @@
 import random
 
+#UPER
+#create table in which word one is key and values are word after
+#next step will be to combine values into lists associated with key
+#once table is made, choose a random start word
+#print the text of key and values to a string.
+#create a stop word based on punctuation or something
+
+
 # Read in all the words in one go
+
 with open("input.txt") as f:
     words = f.read()
 
 # TODO: analyze which words can follow other words
 # Your code here
 
+cache = {}
+words = words.lower().strip().split()
+for i in range(len(words)-1):
+    key = words[i]
+    if key not in cache:
+        # print(True)
+        cache[key] = [words[i+1]]
+    else:
+        # print(False)
+        cache[key].append(words[i+1])
 
 # TODO: construct 5 random sentences
 # Your code here
 
+sentences = [str()]*5
+paragraph = str()
+
+for i in range(len(sentences)):
+    word_item = random.choice(
+        [word for word in words if word[0] == "\"" or word[0].isupper()])
+    while True:
+        sentences[i] += f"{word_item} "
+        if word_item[-1] in [".", "?", "!"]:
+            cap = sentences[i][1:].capitalize()
+            sentences[i] = f"\"{cap}"
+            paragraph += f"{sentences[i].strip()}\" "
+            break
+        word_item = random.choice(cache[word_item])
+        
+print(paragraph)
--- applications/sumdiff/sumdiff.py
+++ applications/sumdiff/sumdiff.py
@@ -7,9 +7,51 @@ f(a) + f(b) = f(c) - f(d)
 #q = set(range(1, 200))
 q = (1, 3, 4, 7, 12)
 
+cache = {}
+sumcache = {}
 
 def f(x):
+    
+    for i in q:
+        cache[i] = f(i)
+    
     return x * 4 + 6
 
+
+#grouped set: left and right side
+{(f(a), f(b)): sum} {(sumf(a)+f(b): (f(a), f(b)))}
+f(a) + F(b)   F(c) - F(d)
+iterate through q
 # Your code here
 
+list1 = f.a + f.b
+list2 = f.c - f.d
+list1 == list2
+
+leftside = {}#addition
+rightside = {}#subtraction with i[1] - i[0]
+
+
+
+z = list(itertools.combination(q, 2))
+
+rightside = {}
+leftside = {}
+
+for x in q:
+    if f(x) + f(x) not in leftside:
+        leftside[(f(x) + f(x))] = list(x, x)
+
+for i in z:
+    #need to include the sum of fi and fi
+    numsum = f(i[0]) + f(i[1])
+    if numsum not in leftside:
+        leftside[numsum] = []
+    leftside[numsum].append(i)
+    
+    negsum = f(i[1]) - f(i[0])
+    if negsum not in rightside:
+        rightside[negsum] = []
+    
+    print(i[0] + i[1], f(i[0]) + f(i[0]) )
+    
